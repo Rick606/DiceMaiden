1622265d776b262afcca3b4e4f2f29d2f1b2886d
Rick606 <ricktg0@gmail.com>
2021-04-17 11:35:40 -0700

    Update dice_maiden_logic.rb

--- dice_maiden_logic.rb
+++ dice_maiden_logic.rb
@@ -20,6 +20,7 @@ def alias_input_pass(input)
       [/\b\d+d%\B/i, "Percentile roll", /\b(\d+)d%\B/i, "\\1d100"], # Roll a d100
       [/\bsp\d+\b/i, "Storypath", /\bsp(\d+)\b/i, "ul \\1d10 ie10 t8"], # storypath system
       [/\b\d+eh\d+\+/i, "Engine Heart", /\b(\d+)eh(\d+)\+/i, "\\1d10 ie10 t\\2"] # Engine Heart, the only thing I added]
+      [/\b\d+eh/i, "Engine Heart default target", /\b(\d+)eh/i, "\\1d10 ie10 t8"] # Engine Heart, the only thing I added]
   ]
 
   @alias_types = []
